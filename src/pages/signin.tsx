b2ce32fe2addb91a54d778add8b2073a4810555a
Pedro da C Jr <mpedrocjr@hotmail.com>
2022-09-27 09:36:58 -0400

    Uninstall Firebase package

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -51,47 +51,43 @@ const SignIn: FC = () => {
           </p>
         </div>
         <div className='mt-2'>
-          {providers?.email && (
-            <div className='mb-12'>
-              <form onSubmit={handleSubmit}>
-                <div className='mb-4'>
-                  <label className='mb-1 block text-base font-medium text-primary dark:text-gray-100'>
-                    Your email
-                  </label>
-                  <input
-                    type='email'
-                    id='email'
-                    name='email'
-                    value={email}
-                    onChange={handleChange}
-                    className='block w-full  rounded-lg border border-gray-600 bg-gray-50 p-2.5 text-base text-primary placeholder-primary'
-                    placeholder='Your email'
-                    required
-                  />
-                </div>
-                <div className='mt-6'>
-                  <button
-                    type='submit'
-                    className='group relative flex w-full items-center justify-center rounded-md border border-transparent bg-primary py-2 px-4 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-amber-700'>
-                    <span className='ml-2'>Sign in</span>
-                  </button>
-                </div>
-              </form>
-            </div>
-          )}
-          {providers?.google && (
-            <>
-              <button
-                type='button'
-                onClick={() => signIn(providers.google.id)}
-                className='group relative flex w-full items-center justify-center rounded-md border border-transparent bg-primary py-2 px-4 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-amber-700'>
-                <span>
-                  <FaGoogle className='h-6 w-6' />
-                </span>
-                <span className='ml-2'>Continue with Google</span>
-              </button>
-            </>
-          )}
+          <div className='mb-12'>
+            <form onSubmit={handleSubmit}>
+              <div className='mb-4'>
+                <label className='mb-1 block text-base font-medium text-primary dark:text-gray-100'>
+                  Your email
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  name='email'
+                  value={email}
+                  onChange={handleChange}
+                  className='block w-full  rounded-lg border border-gray-600 bg-gray-50 p-2.5 text-base text-primary placeholder-primary'
+                  placeholder='Your email'
+                  required
+                />
+              </div>
+              <div className='mt-6'>
+                <button
+                  type='submit'
+                  className='group relative flex w-full items-center justify-center rounded-md border border-transparent bg-primary py-2 px-4 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-amber-700'>
+                  <span className='ml-2'>Sign in</span>
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <button
+            type='button'
+            onClick={() => signIn(providers.google.id)}
+            className='group relative flex w-full items-center justify-center rounded-md border border-transparent bg-primary py-2 px-4 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-amber-700'>
+            <span>
+              <FaGoogle className='h-6 w-6' />
+            </span>
+            <span className='ml-2'>Continue with Google</span>
+          </button>
+
           {/* <button
           type='button'
           onClick={signInWithGithubApp}
