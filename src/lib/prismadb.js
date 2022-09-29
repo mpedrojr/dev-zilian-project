8277a568034c887071e9fb9691db5db960b14cb6
Pedro da C Jr <mpedrocjr@hotmail.com>
2022-09-28 21:26:02 -0400

    Auth update

--- src/lib/prismadb.js
+++ src/lib/prismadb.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+const client = global.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== "production") global.prisma = client
 
 export default client
