c8be9d9ddf868369e4cd215f0d187c8fcadfcf4f
Pedro da C Jr <mpedrocjr@hotmail.com>
2022-09-29 08:33:04 -0400

    Auth update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-export const authOptions = {
-  // Configure one or more authentication providers
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '../../../lib/client';
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+});
