263ea2d6deede74b9d97404965c10ae2c5d554e7
Pedro da C Jr <mpedrocjr@hotmail.com>
2022-09-30 14:27:22 -0400

    Auth update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -46,8 +46,8 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
     TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
       version: "2.0", // opt-in to Twitter OAuth 2.0
     }),
     // ...add more providers here
